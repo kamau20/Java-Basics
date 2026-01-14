ef3c471771d2ccbed01ecd3acce8d56932b28dd3
kamau <kamauelisha8@gmail.com>
2026-01-14 13:19:02 +0300

    Initial commit - Java basics project

---
+++ src/main/java/PersonalIntroduction.java
@@ -0,0 +1,7 @@
+public class PersonalIntroduction {
+    public static void main(String[] args) {
+        System.out.println("My name is Elisha Kamau");
+        System.out.println("My goal is to become a certified Java Developer");
+        System.out.println("I am excited and looking forward to learn Java and Springboot framework");
+    }
+}
