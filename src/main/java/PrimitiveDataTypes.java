1128c172e69455605445416380c1ea6b62f678a2
kamau <kamauelisha8@gmail.com>
2026-01-14 13:50:46 +0300

    Initial commit - Java basics project

---
+++ src/main/java/PrimitiveDataTypes.java
@@ -0,0 +1,18 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        // Integer types
+        int age = 25;
+        long bigNumber = 123456789L;
+
+        // Decimal types
+        float price = 19.99f;
+        double precise = 3.1415;
+
+        //Character and boolean
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+}
