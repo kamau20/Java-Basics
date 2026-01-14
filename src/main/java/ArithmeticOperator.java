3578e8a9b272560379843e0c580acf97782fc74f
kamau <kamauelisha8@gmail.com>
2026-01-14 14:07:17 +0300

    Initial commit - Java basics project

---
+++ src/main/java/ArithmeticOperator.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a = 15, b =4;
+
+        System.out.println("a + b = " + (a + b)); // 19
+        System.out.println("a - b = " + (a - b)); // 11
+        System.out.println("a * b = " + (a * b)); // 60
+        System.out.println("a / b = " + (a / b)); // 3 (integer division)
+    }
+}
