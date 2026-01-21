d25d9a86787715bdecd8433190d0ddf40724cc9f
kamau <kamauelisha8@gmail.com>
2026-01-21 09:51:33 +0300

    Assesments

--- src/main/java/com/github/kamau20/Assessments/Assessment1_ElishaKamau.java
+++ src/main/java/com/github/kamau20/Assessments/Assessment1_ElishaKamau.java
@@ -3,6 +3,28 @@ package com.github.kamau20.Assessments;
 public class Assessment1_ElishaKamau {
     public static void main(String[] args) {
 
+        variableOperations(); // Quiz 1
+        // gradeCalculator(); // Quiz 2
+        // numberSequence(); // Quiz 3
+        // inputValidation(); // Quiz 4
+        // patternPrinting(); // Quiz 5
     }
 
+    public static void variableOperations() {
+        // Solution for Quiz Variable Operations
+
+        int a = 15;
+        int b = 4;
+        int c = 7;
+
+        int sum = a + b + c; // Calculates the sum
+        int product = a * b; // Calculates the product
+        int expressionResult = (a + b) * c; // Calculates the expressionResult
+        int remainder = a % b; // Calculates the remainder
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Expression Result: " + expressionResult);
+        System.out.println("Remainder: " + remainder);
+    }
 }
