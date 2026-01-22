bfc43cbeb11b9dd5be05d733c27b428e7a119d85
kamau <kamauelisha8@gmail.com>
2026-01-22 09:00:25 +0300

    Java basics project

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,48 @@
+public class BankAccount {
+    public String accountNumber;
+    public String accountHolder;
+    public double balance;
+
+    // Default Constructor
+    public BankAccount() {
+        accountNumber = "Unknown";
+        accountHolder = "Unknown";
+        balance = 0.0;
+    }
+
+    // Constructor with Account Info
+    public BankAccount(String number, String holder) {
+        accountNumber = number;
+        accountHolder = holder;
+        balance = 0.0;
+    }
+
+    // Constructor with all properties
+    public BankAccount(String number, String holder, double initialBalance) {
+        accountNumber = number;
+        accountHolder = holder;
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Account: " + accountNumber);
+        System.out.println("Holder: " + accountHolder);
+        System.out.println("Balance: $" + balance);
+    }
+}
---
+++ src/main/java/Objects.java
@@ -0,0 +1,5 @@
+public class Objects {
+    public class Car {
+
+    }
+}
