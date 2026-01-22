91eb0b0e2de847d12b6073f399a6e0f67f0ca8e3
kamau <kamauelisha8@gmail.com>
2026-01-22 14:26:40 +0300

    Exercises

--- src/main/java/Exercises/EmployeeMain.java
+++ src/main/java/Exercises/EmployeeMain.java
@@ -2,10 +2,12 @@ package Exercises;
 
 public class EmployeeMain {
     public static void main(String[] args) {
-        Employee employee1 = new Employee();
-        Employee employee2 = new Employee();
 
+        Employee employee1 = new Employee();
+        employee1.name = "Elisha Kamau";
+        employee1.salary = 50000;
+        employee1.calculateBonus(0.05);
         employee1.printInfo();
-        employee2.printInfo();
+
     }
 }
