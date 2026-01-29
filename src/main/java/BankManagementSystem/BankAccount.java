5203e85b4a83c13cbfe162af03572ed5ee2a263c
kamau <kamauelisha8@gmail.com>
2026-01-29 14:57:41 +0300

    Bank Management System

--- src/main/java/BankManagementSystem/BankAccount.java
+++ src/main/java/BankManagementSystem/BankAccount.java
@@ -11,4 +11,22 @@ public class BankAccount {
         this.accountHolder = accountHolder;
         this.balance = balance;
     }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println(amount + " deposited. New balance is: " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println(amount + " withdraw. New balance is: " + balance);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("Balance is: " + balance);
+    }
 }
