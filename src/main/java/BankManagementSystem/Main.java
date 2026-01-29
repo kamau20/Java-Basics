3d03a30c42758cb39a8dc28bb679f47e34ca4655
kamau <kamauelisha8@gmail.com>
2026-01-29 16:16:26 +0300

    Bank Management System

--- src/main/java/BankManagementSystem/Main.java
+++ src/main/java/BankManagementSystem/Main.java
@@ -2,6 +2,21 @@ package BankManagementSystem;
 
 public class Main {
     public static void main(String[] args) {
+        // Creating Objects
+        Bank bank = new Bank();
 
+        SavingsAccount savings =  new SavingsAccount("EK1234", "Elisha Kamau", 5000, 2.5);
+        CurrentAccount current = new CurrentAccount("CA456", "Salome Kamau", 3000, 1.5);
+
+        // Adding Accounts into the Bank System
+        bank.addAccount(current);
+        bank.addAccount(savings);
+
+        savings.deposit(1000);
+        current.withdraw(1000);
+
+        savings.addInterest();
+
+        bank.transferFund("CA456", "Salome Kamau", 500);
     }
 }
