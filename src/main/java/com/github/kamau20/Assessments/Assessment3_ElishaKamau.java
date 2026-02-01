839a7392b2ca851920b5fe662ad00a0e0df4b15c
kamau <kamauelisha8@gmail.com>
2026-02-01 12:00:52 +0300

    Assessments

---
+++ src/main/java/com/github/kamau20/Assessments/Assessment3_ElishaKamau.java
@@ -0,0 +1,34 @@
+package com.github.kamau20.Assessments;
+
+public class Assessment3_ElishaKamau {
+    public static void main(String[] args) {
+
+        // PROBLEM 1
+        // LibraryItem {}
+        // Book {}
+        // DVD {}
+        // Magazine {}
+        // LibraryTest {}
+
+        // PROBLEM 2
+        // BankAccount {}
+        // InsufficientFundsException {}
+        // InvalidTransactionException {}
+        // AccountNotFoundException {}
+        // BankTest {}
+
+        // PROBLEM 3
+        // Product {}
+        // Electronics {}
+        // Clothing {}
+        // Books {}
+        // InvalidProductException {}
+        // OutOfStockException {}
+        // EcommerceTest {}
+
+        // PROBLEM 4
+        // EmploymentManagement {}
+        // Analysis_YourName.txt
+
+    }
+}
